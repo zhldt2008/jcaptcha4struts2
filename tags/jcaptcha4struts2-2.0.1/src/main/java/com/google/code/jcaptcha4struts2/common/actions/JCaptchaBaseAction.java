6953d2d77404a2035b0d8db8b5e1a9aa37c7e886
yohanliyanage <yohanliyanage@451dba72-8557-11dd-9899-c7ba65f7c4c7>
2009-08-09 11:27:47 +0000

    [maven-release-plugin]  copy for tag jcaptcha4struts2-2.0.1

---
+++ tags/jcaptcha4struts2-2.0.1/src/main/java/com/google/code/jcaptcha4struts2/common/actions/JCaptchaBaseAction.java
@@ -0,0 +1,172 @@
+/*
+ * Copyright (C) 2008-2009 Yohan Liyanage. 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+package com.google.code.jcaptcha4struts2.common.actions;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.google.code.jcaptcha4struts2.core.validation.JCaptchaValidator;
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * This Action Class provides necessary functionality for handling JCaptcha content. This is to be
+ * used as the superclass of an action class which handles JCaptcha related forms, if intercepters
+ * are not being used.
+ * <p>
+ * <b>Note :</b> If intercepters are used, <b>do not use</b> this class as the base class. It would
+ * result in exceptions (due to double validation of captcha).
+ * 
+ * @author Yohan Liyanage
+ * @since 1.0
+ * @version 2.0
+ */
+public class JCaptchaBaseAction extends ActionSupport {
+
+    private static final Log LOG = LogFactory.getLog(JCaptchaBaseAction.class);
+
+    private static final long serialVersionUID = -5245380448623189946L;
+
+    /**
+     * Stores the actual captcha response.
+     */
+    private String jCaptchaResponse;
+
+    /**
+     * Flag whether validation should be done.
+     */
+    private boolean validationEnabled = true;
+
+    /**
+     * No-args constructor. Builds a JCaptchaBaseAction which does the validation automatically
+     * through {@link #validate()} method.
+     */
+    public JCaptchaBaseAction() {
+        super();
+    }
+
+    /**
+     * Constructs a JCaptchaBaseAction which allows to enable / disable automatic validation through
+     * {@link #validate()}. If disabled, implementor is required to manually invoke
+     * {@link #doValidateCaptcha()} method for validation.
+     * <p>
+     * Subclasses may utilize this constructor to disable automatic validation.
+     * 
+     * @param validationEnabled
+     *            enable / disable auto-validation
+     */
+    public JCaptchaBaseAction(boolean validationEnabled) {
+        super();
+        this.validationEnabled = validationEnabled;
+    }
+
+    /**
+     * Returns JCaptcha response by user.
+     * 
+     * @return jcaptcha response by user
+     */
+    public String getJCaptchaResponse() {
+        return jCaptchaResponse;
+    }
+
+    /**
+     * Sets JCaptcha response by user.
+     * 
+     * @param jCaptchaResponse
+     *            jcaptcha response by user
+     */
+    public void setJCaptchaResponse(String jCaptchaResponse) {
+        this.jCaptchaResponse = jCaptchaResponse;
+    }
+
+    /**
+     * Validates the JCaptcha response and adds a field error if validation fails, given that the
+     * validation is enabled (default case).
+     * <p>
+     * This method is final, and is not overridable. To provide your own validation logic, use the
+     * {@link #validateInput()} method.
+     */
+    public final void validate() {
+
+        if (validationEnabled) {
+            doValidateCaptcha();
+        } else {
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("Skipping validation of captcha as validate=false");
+            }
+        }
+
+        // Invoke user validation routines
+        validateInput();
+    }
+
+    /**
+     * Validates the JCaptcha response and adds a field error if validation fails.
+     */
+    protected void doValidateCaptcha() {
+
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Validating  : " + jCaptchaResponse);
+        }
+
+        if (!JCaptchaValidator.validate()) {
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("validation failed (input string : '" + jCaptchaResponse
+                        + "'), field error added");
+            }
+            addFieldError("j_captcha_response", getValidationErrorMessage());
+        }
+    }
+
+    /**
+     * Returns the error message text to be displayed if captcha validation fails.
+     * <p>
+     * Developers may override this method to provide custom messages.
+     * 
+     * @return error message to be displayed if captcha validation fails.
+     */
+    protected String getValidationErrorMessage() {
+        return "Entered string does not match with image";
+    }
+
+    /**
+     * Implementers may override this method to provide validation logic, instead of standard
+     * validate() method.
+     * <p>
+     * This method will be invoked from the validate() method, which is final in this class, and not
+     * overridable.
+     */
+    public void validateInput() {
+        // No-implementation
+    }
+
+    /**
+     * Returns true if automatic validation is enabled.
+     * 
+     * @return true if automatic validation is enabled.
+     */
+    public boolean isValidationEnabled() {
+        return validationEnabled;
+    }
+
+    /**
+     * Sets whether automatic validation is enabled.
+     * 
+     * @param validationEnabled if true, automatic validation will be enabled.
+     */
+    public void setValidationEnabled(boolean validationEnabled) {
+        this.validationEnabled = validationEnabled;
+    }
+
+    
+}
---
+++ tags/jcaptcha4struts2-2.0.1/src/main/java/com/google/code/jcaptcha4struts2/core/actions/JCaptchaImageAction.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright (C) 2008-2009 Yohan Liyanage. 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+package com.google.code.jcaptcha4struts2.core.actions;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.struts2.ServletActionContext;
+
+import com.google.code.jcaptcha4struts2.core.beans.JC4S2Config;
+import com.octo.captcha.service.image.ImageCaptchaService;
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * This Struts2 Action generates the Captcha Image.
+ * <p>
+ * It utilizes the JCaptcha's Image Service to generate JPEG Stream for Captcha Image.
+ * 
+ * @author Yohan Liyanage
+ * @since 1.0
+ * @version 2.0
+ */
+public class JCaptchaImageAction extends ActionSupport {
+
+    private static final String IMAGE_FORMAT = "jpg";
+    private static final long serialVersionUID = 563498642053643243L;
+    private static final Log LOG = LogFactory.getLog(JCaptchaImageAction.class);
+
+    /**
+     * Holds the byte[] for JPEG encoded Captcha Image.
+     */
+    private byte[] captchaImage;
+
+    /**
+     * Action execution logic, which generates the Captcha Image Stream and sets the JPEG encoded
+     * byte stream to captchaImage field.
+     * <p>
+     * Subsequent to invocation of this method, the generated captcha is available through
+     * {@link #getCaptchaImage()} method.
+     * 
+     * @return action forward string
+     * 
+     * @throws Exception
+     */
+    public String execute() {
+
+        ByteArrayOutputStream imageOut = new ByteArrayOutputStream();
+        HttpServletRequest request = ServletActionContext.getRequest();
+
+        // Captcha Id is the session ID
+        String captchaId = request.getSession().getId();
+
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Generating Captcha Image for SessionID : " + captchaId);
+        }
+
+        // Generate Captcha Image
+        BufferedImage image =
+                getImageCaptchaService().getImageChallengeForID(captchaId, request.getLocale());
+
+        // Encode to JPEG Stream
+        try {
+            ImageIO.write(image, IMAGE_FORMAT, imageOut);
+        } catch (IOException e) {
+            LOG.error("Unable to JPEG encode the Captcha Image due to IOException", e);
+            throw new IllegalArgumentException("Unable to JPEG encode the Captcha Image", e);
+        }
+
+        // Get byte[] for image
+        captchaImage = imageOut.toByteArray();
+
+        return SUCCESS;
+    }
+
+    /**
+     * Returns the byte stream for JPEG encoded captcha image.
+     * 
+     * @return byte stream for JPEG encoded captcha image
+     */
+    public byte[] getCaptchaImage() {
+        return captchaImage;
+    }
+
+    /**
+     * Returns the ImageCaptchaService implementation, obtained via the {@link JC4S2Config}.
+     * 
+     * @return image captcha service implementation.
+     */
+    protected ImageCaptchaService getImageCaptchaService() {
+        return JC4S2Config.getInstance().getImageCaptchaService();
+    }
+}
